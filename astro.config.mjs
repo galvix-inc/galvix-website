de47f4fa8b0bc41f0070f73839966b8c95b85d30
Piyush Agrawal <piyush.agrawal@reddit.com>
2023-12-19 13:15:35 -0500

    Added mixpanel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,26 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.galvix.com',
-  integrations: [tailwind(), react(), sitemap({
-    filter: (page) => 
-      page !== 'https://www.galvix.com/sandbox/' && 
-      page !== 'https://www.galvix.com/index-saas/',
-  })],
+  integrations: [
+    tailwind(), 
+    react(), 
+    sitemap({
+      filter: (page) => 
+        page !== 'https://www.galvix.com/sandbox/' && 
+        page !== 'https://www.galvix.com/index-saas/',
+    }),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   redirects: {
     "/resources/[slug]": "/article/[slug]",
     "/free-nexus-study": "/get-started",
