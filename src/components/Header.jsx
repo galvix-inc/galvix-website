a9dc36f055b88326898b1af800b7925556d42592
Piyush Agrawal <piyush.agrawal@reddit.com>
2024-01-17 16:21:51 -0500

    Updated header shadow

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="bg-white sticky top-0 z-50">
+    <header className="bg-white sticky top-0 z-50 shadow-sm">
       <nav className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="/" className="-m-1.5 p-1.5">
