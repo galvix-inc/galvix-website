c04617e3f4ec036bcbde12706827f576bb659af1
shad zaidi <shadzaidi@shads-MacBook-Air.local>
2024-08-21 18:19:04 +0530

    href for mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -86,6 +86,7 @@ export default function Header() {
                   <a
                     key={item.name}
                     href={item.href}
+                    onClick={() => setMobileMenuOpen(false)}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     {item.name}
